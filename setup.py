6bbb994fbf31d5da09989a0592f56ff52ea5327e
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-22 14:15:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
-
-setup(name='parspy',
-      version='0.0.1',
-      description='A lightweight parser library.',
-      author='PlaceReporter99',
-      author_email='placereporter99pypi@outlook.com',
-      url='https://github.com/PlaceReporter99/parspy',
-      packages=[],
-     )
+try:
+      setup(name='parspy',
+            version='0.0.1',
+            description='A lightweight parser library.',
+            author='PlaceReporter99',
+            author_email='placereporter99pypi@outlook.com',
+            url='https://github.com/PlaceReporter99/parspy',
+            packages=[],
+           )
+except:
+      pass
